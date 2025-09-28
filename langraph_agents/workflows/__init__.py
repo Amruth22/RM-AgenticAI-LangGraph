5f67d6c4b9bd0747d48508e072f6abdcf7c9c57e
Amruth22 <89969774+Amruth22@users.noreply.github.com>
2025-09-28 16:24:19 +0530

    Fix import error by removing non-existent workflow imports

--- langraph_agents/workflows/__init__.py
+++ langraph_agents/workflows/__init__.py
@@ -1,11 +1,13 @@
 """LangGraph workflows for orchestrating multi-agent processes."""
 
 from .prospect_analysis_workflow import ProspectAnalysisWorkflow
-from .product_recommendation_workflow import ProductRecommendationWorkflow
-from .interactive_chat_workflow import InteractiveChatWorkflow
+
+# TODO: Implement these workflows in future versions
+# from .product_recommendation_workflow import ProductRecommendationWorkflow
+# from .interactive_chat_workflow import InteractiveChatWorkflow
 
 __all__ = [
     "ProspectAnalysisWorkflow",
-    "ProductRecommendationWorkflow", 
-    "InteractiveChatWorkflow"
+    # "ProductRecommendationWorkflow", 
+    # "InteractiveChatWorkflow"
 ]
